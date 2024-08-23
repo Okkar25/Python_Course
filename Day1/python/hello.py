21d273cf8203c9188fc0d8b94f801f10bc8e703f
OkkarShopDoora <okkaraung.mhs@gmail.com>
2024-08-23 15:37:42 +0630

    Day1

---
+++ Day1/python/hello.py
@@ -0,0 +1,56 @@
+name = "234"
+# print(type(name))
+
+num = 23
+float = 23.5 
+
+list = [1,2,3,4,5]
+
+tuple = (1,2,3,4,5)
+
+# range(start, end , step)
+# start / step => optional 
+#  end => must
+
+range(5)
+
+set = {1,2,3,4,5}
+
+print(type(set))
+
+# ctrl + / => comment
+# js => object 
+# python => dictionary => dict 
+# key - value 
+
+saw_info = {
+   "name": "Saw Yadana",
+   "age": 23,
+    "RS": False,
+}
+
+print(saw_info)
+
+#  boolean 
+# bytes / bytearray , memoryview 
+
+money = None
+# print(type(money))
+
+
+# naming convention
+
+
+
+# camelCase 
+myNameIs = "Saw Yadana"
+
+# snake_case 
+my_name_is = "Okkar"
+
+# PascalCase 
+MyNameIs = "Yadana"
+
+# kebab-case
+# my-name-is = "Aung"
+
