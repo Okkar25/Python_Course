35e15dd26dfb3caebad15436e25bf3917370c699
Okkar25 <okkaaung2508@gmail.com>
2024-08-29 21:06:43 +0630

    nested if problems addded for Day4

--- Day4/problems.py
+++ Day4/problems.py
@@ -106,7 +106,53 @@ print(f"The triangle is {type} triangle")
 
 
 
-# -------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------
 
 
 
+# Problem 6: Discount Calculation Based on Price
+# Problem: Write a Python program that takes the price of an item as input and calculates the final price after applying a discount using nested if statements.
+
+# for price > 200 => discount 20%
+# for price > 100 => discount 10%
+# for price < 100 => discount 5%
+
+
+# ==============================================================
+
+
+# Problem 7: University Admission Decision
+
+# Problem: Write a Python program to determine a student's admission status based on several criteria. The criteria include academic performance, extracurricular activities, volunteer work, and entrance exam scores.
+
+
+# The program should prompt the user for their GPA, number of extracurricular activities, volunteer work hours, and entrance exam score. Based on the following conditions, the program should decide if the student gets "Full Admission", "Conditional Admission", or "No Admission":
+
+
+# Full Admission: -------------------
+# GPA is 3.5 or higher.
+# If GPA is 3.5 or higher:
+# Entrance exam score must be 85 or higher.
+# If entrance exam score is 85 or higher:
+# At least 2 extracurricular activities.
+# If 2 or more extracurricular activities:
+# At least 30 volunteer work hours.
+
+# Conditional Admission: ------------
+# If the GPA is between 3.0 and 3.49 and the entrance exam score is between 75 and 84.
+# If so:
+# At least 1 extracurricular activity or 20 volunteer work hours.
+
+# No Admission: ---------------------
+# If none of the above conditions are met.
+
+
+
+# =======================================================================================
+
+
+
+# Problem 8: You are a customer at a restaurant to order something to eat. But you will decide what to eat depending on your balance. Write a python program to determine what to eat regrading the amount of your balance. 
+
+# You can order (pizza for $300 / steak for $200 / mac and cheese for $100 ) 
+# After you have purchase one meal and if you still have the sufficient fund, you can continue top order remaining meals. The program should prompt to ask for permission from customer if he / she still want to continue to order after one meal. After the ordering process, the program should output the remaining balance at the end. 
