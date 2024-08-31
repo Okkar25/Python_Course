887f57ab31556a8085ccaa7f897ee726493c5438
Okkar25 <okkaaung2508@gmail.com>
2024-09-01 01:30:23 +0630

    Day6 string methods and while loop

---
+++ Day6/main.py
@@ -0,0 +1,104 @@
+# while loop 
+
+age = 0 # 10
+
+# while age < 10:
+#   # if age == 0: print(age)
+#   print(age) 
+#   age += 1   
+# print("final age",age)
+
+
+# age = 1 
+
+# while age < 10:
+
+#   print(age)
+#   if age == 5:
+#     break
+#   age += 1
+
+# age = 0 
+
+# while age < 10: # 4
+  
+#   print(age)
+#   age += 1
+#   if age == 5:
+#     continue
+    
+# age = 0
+
+# while age < 10: 
+#   pass
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# print("i is no longer less than 6")
+  
+  
+  
+# --------------------------------------------------------------------------------------------
+
+
+
+# name = input("Enter your name : ")
+
+# while name == "":
+#    name = input("Enter your name : ")
+
+# age = input("Enter your age : ")
+
+# while not age.isdigit() or int(age) < 1 :
+#     age = input("Enter your age : ")
+
+# print(f"Hello {name}!")
+# print(f"You are {age} years old")
+
+
+# 1. Print Numbers from 1 to 10
+
+# number = 1
+
+# while number < 10:
+#    print(number)
+#    number += 1
+#    if number == 10: print(number)
+
+   
+# 2 Write a Python program to find the sum of natural numbers up to a given number n.
+
+
+# start = 1
+# n = 7
+# sum = 0
+
+# while start <= n: 
+#    if start == 5:
+#       start += 1
+
+#    # start = 6
+
+#    print(start)   
+#    sum += start 
+#    start += 1 
+   
+# print(sum)
+
+
+
+
+
+
---
+++ Day6/problem.py
@@ -0,0 +1,92 @@
+
+
+# name = "Saw Yadana"
+# reversed_name = name[::-1]
+
+# print(reversed_name)
+
+# for i in range(len(name) - 1, -1, -1):
+#   reversed_name += name[i]
+
+# print(reversed_name)
+
+
+
+
+# name = "sawydanar1933@gmail.com"
+# new_name = name[-10:]
+# split_name = name.split("@")
+# print(new_name)
+
+
+
+# language = "Python is fun"
+# language_list = language.split()
+# join_language = "-".join(language_list)
+# join_language = language.replace(" ", "-")
+# print(join_language)
+
+# name = "The Fox jumps into the water"
+# new_name = name.split(" ")
+# print(new_name[0])
+# print(new_name[len(new_name) - 1])
+
+name = "Saw Yadana"
+# new_name = name.replace("Saw", "S.")
+
+new_name = name.split(" ")
+first_name = new_name[0]
+last_name = new_name[1]
+
+# print(f"{new_name[0][0]}. {last_name}")
+
+# array / list => 2 dimensional array 
+
+
+# modify
+# .upper()
+# .lower()
+# .strip()
+# .capitalize()
+# .replace()
+# .split()
+# .join()
+# [::-1]
+
+# .count()
+# .find()
+# .index()
+# .isdigit()
+# .isnumeric()
+# .islower()
+# .isupper()
+# .swapcase()
+# .zfill()
+
+name = "80000" # Saw YaDana
+
+# print(name.count("a"))
+
+# print(name.find("a"))
+# print(name.index("d"))
+
+# print(name.isdigit())
+# print(name.isnumeric())
+
+# print(name.upper().isupper())
+
+# print(name.swapcase())
+
+# name_d_index = name.find("d")
+# name_D = name[name.find("d")].upper()
+# desired_name = name.replace("d", name_D)
+# print(desired_name)
+
+print(name.zfill(8))
+
+
+# https://www.geeksforgeeks.org/python-string-methods/
+
+# https://www.w3schools.com/python/python_ref_string.asp
+
+
